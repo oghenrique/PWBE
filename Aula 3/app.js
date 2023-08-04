0ab451ddc0e6c44dd61083090cd20c2c38f68809
Gustavo <23111732@CFP127ALUNO.LOCAL>
2023-08-04 16:49:24 -0300

    finalizado

--- Aula 3/app.js
+++ Aula 3/app.js
@@ -12,20 +12,23 @@ var entradaDeDados = readline.createInterface({
     output: process.stdout
 })
 
+entradaDeDados.question('Digite seu nome: ', function(nomeUsuario){
+let nome = nomeUsuario 
+
 entradaDeDados.question('Digite a primeira nota: ', function(primeiraNota){
-    var nota1 = primeiraNota 
+    let nota1 = primeiraNota 
     var n1 = parseInt(nota1)
 
     entradaDeDados.question('Digite a segunda nota: ', function(segundaNota){
-        var nota2 = segundaNota 
+        let nota2 = segundaNota 
         var n2 = parseInt(nota2)
 
             entradaDeDados.question('Digite a terceira nota: ', function(terceiraNota){
-            var nota3 = terceiraNota 
+            let nota3 = terceiraNota 
             var n3 = parseInt(nota3)
 
             entradaDeDados.question('Digite a quarta nota: ', function(quartaNota){
-                var nota4 = quartaNota
+                let nota4 = quartaNota
                 var n4 = parseInt(nota4)
 
                 var media = (n1 + n2 + n3 + n4)/4 ;
@@ -36,10 +39,11 @@ entradaDeDados.question('Digite a primeira nota: ', function(primeiraNota){
     console.log('A terceira nota é:  ' + nota3)
     console.log('A quarta nota é:  ' + nota4)
     console.log('')
-    console.log('A média é:  ' + media)
+    console.log('Olá, ' + nome + 'a sua média é:  ' + media)
 
     entradaDeDados.close()
-            
+                
+                })
             })
         })
     })
