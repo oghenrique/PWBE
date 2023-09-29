c2a5d95cedc45a0ec9a2b1ce3a2823a8b0d30448
Gustavo <santosguhenrique@gmail.com>
2023-09-29 13:42:09 -0300

    fazendo exercicio 5

---
+++ lista de exercicios/EXERCICIO 5/app.js
@@ -0,0 +1,28 @@
+/*********************************************************************************
+ * Objetivo: Realizar o cálculo de uma tabuada conforme requisitos específicos
+ * Autor: Gustavo Henrique
+ * Data: 29/09/2023
+ * Versão: 1.0
+**********************************************************************************/
+
+var gerenciarNumeros = require('./modulo/parImpar.js')
+var readline = require('readline')
+
+var entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+entradaDeDados.question('Insira um numero inicial entre 0 e 500', function(valorInicial){
+    let valorIni = valorInicial
+
+    entradaDeDados.question('Insira o número final entre 100 e 1000', function(valorFinal){
+        let valorFin = valorFinal
+
+        entradaDeDados.question('Escolha como separar os números [1- Pares | 2- Ímpares | 3- Ambos]', function (escolhaUsuario){
+            let escolha = escolhaUsuario
+
+            gerenciarNumeros.verificarValores(valorIni, valorFin, escolha)
+        })
+    })
+})
---
+++ lista de exercicios/EXERCICIO 5/modulo/parImpar.js
@@ -0,0 +1,6 @@
+/*********************************************************************************
+ * Objetivo: Realizar o cálculo de uma tabuada conforme requisitos específicos
+ * Autor: Gustavo Henrique
+ * Data: 29/09/2023
+ * Versão: 1.0
+**********************************************************************************/
