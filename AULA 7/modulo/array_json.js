daecdfd5d3e2152b3b6bd56f1f5aa1eb4df13699
Gustavo <santosguhenrique@gmail.com>
2023-10-06 16:47:58 -0300

    testando json e array

--- AULA 7/modulo/array_json.js
+++ AULA 7/modulo/array_json.js
@@ -73,15 +73,15 @@ const filtrarProduto = function (nomeProduto) {
     let nome = nomeProduto
 
     let status = false
-   
+
     // if (listaDeProdutos.indexOf(nome) >= 0)
     //     status = true
     // else status = false
 
     // return listaDeProdutos.includes(nome)
 
-    listaDeProdutos.forEach(function(produto){
-        if(produto.toUpperCase == nome.toUpperCase)
+    listaDeProdutos.forEach(function (produto) {
+        if (produto.toUpperCase == nome.toUpperCase)
             status = true
 
     })
@@ -89,7 +89,7 @@ const filtrarProduto = function (nomeProduto) {
     return status
 }
 
-const manipulandoArrayJson = function(){
+const manipulandoArrayJson = function () {
     /*
     [] -> Array
     {} -> JSON
@@ -97,18 +97,87 @@ const manipulandoArrayJson = function(){
             {nome: 'josé', telefone: '123456789'}
     */
 
-            let contato = {nome: 'José da Silva', telefone: '123456789', email: 'jose@gmail.com'}
-            let contato2 = {nome: 'Maria da Silva', telefone: '87654', email: 'maria@gmail.com'}
-            let contato3 = {nome: 'João da Silva', telefone: '76733455', email: 'joao@gmail.com'}
+    let contato = { nome: 'José da Silva', telefone: '123456789', email: 'jose@gmail.com' }
+    let contato2 = { nome: 'Maria da Silva', telefone: '87654', email: 'maria@gmail.com' }
+    let contato3 = { nome: 'João da Silva', telefone: '76733455', email: 'joao@gmail.com' }
+
+    //Adicionando elementos individuais nos objetos JSON
+    contato.celular = '988009363'
+    contato.data_nascimento = '2007-10-09'
+
+    contato2.celular = '543654486'
 
 
-            //Criando um array de dados com o JSON
-            let arrayContatos = [contato, contato2, contato3]
+    //Criando um array de dados com o JSON
+    let arrayContatos = [contato, contato2, contato3]
 
 
-            console.table(arrayContatos)
+    // console.table(arrayContatos)
+    // console.log(arrayContatos[1].nome)
+
+    arrayContatos.forEach(function (dadosContato) {
+        console.log('Nome: ' + dadosContato.nome)
+        console.log('Telefone: ' + dadosContato.telefone)
+        console.log('Email: ' + dadosContato.email)
+        //tratamento de dados para quando não houver conteudo
+        if (dadosContato.celular != undefined)
+            console.log('Celular: ' + dadosContato.celular)
+        if (dadosContato.data_nascimento != undefined)
+            console.log('Data de nascimento: ' + dadosContato.data_nascimento)
+
+        console.log('################################# \n')
+    })
 }
 
-manipulandoArrayJson()
+const cadastrarProdutos = function () {
+    let listaDeMarcas = [
+        { id: 1, nome_marca: 'Dell' },
+        { id: 2, nome_marca: 'Apple' },
+        { id: 3, nome_marca: 'Lenovo' },
+        { id: 4, nome_marca: 'Positivo' },
+        { id: 5, nome_marca: 'Razer' },
+        { id: 6, nome_marca: 'Logitech' },
+        { id: 7, nome_marca: 'Multilaser' }
+    ]
+
+    let listaDeCategorias = [
+        { id: 1, nome_categoria: 'Periferico', descricao_categoria: 'Perifericos de computadores' },
+        { id: 2, nome_categoria: 'Hardware', descricao_categoria: 'Hardware para montar computadores' },
+        { id: 3, nome_categoria: 'Computador', descricao_categoria: 'Computadores completos' },
+        { id: 4, nome_categoria: 'Games', descricao_categoria: 'Acessorios para games' }
+    ]
+
+    let listaDeSituacao = [
+        { id: 1, situacao: 'Novo' },
+        { id: 2, situacao: 'Semi-Novo' },
+
+    ]
+
+    let listaDeCores = [
+        { id: 1, nome_cor: 'Branco' },
+        { id: 1, nome_cor: 'Preto' },
+        { id: 1, nome_cor: 'Cinza' },
+        { id: 1, nome_cor: 'Vermelho' },
+        { id: 1, nome_cor: 'Azul' }
+    ]
+
+    let produto1 = {
+        nome: 'Mouse',
+        descricao: 'Mouse optico com fio',
+        qtde: 50,
+        valor: 60.00,
+        marca: listaDeMarcas[0],
+        categoria: listaDeCategorias[0],
+        situacao: listaDeSituacao[0],
+        cor: [
+            listaDeCores[0],
+            listaDeCores[1],
+            listaDeCores[2]
+
+            ]
+    }
+}
+
+// manipulandoArrayJson()
 // percorrendoArray()
 // introducaoArray()
